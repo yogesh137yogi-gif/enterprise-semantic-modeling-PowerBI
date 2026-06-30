a75b5aa027b4657b578882fc26ee8e9e09c21df0
yogesh-baki <yogesh137.yogi@gmail.com>
2026-07-01 00:47:01 +0530

    Update 01_Create_Database.sql

--- SQL/01_Create_Database.sql
+++ SQL/01_Create_Database.sql
@@ -1,1 +1,23 @@
+/*
+===============================================================================
+ Project      : Enterprise Semantic Modeling - Power BI
+ Company      : AutoNova Motors
+ Author       : Yogesh Baki
+ Description  : Creates the Enterprise Semantic Modeling database.
+===============================================================================
+*/
 
+-- Check if the database already exists 
+IF DB_ID('AutoNovaDW') IS NOT NULL
+BEGIN
+    DROP DATABASE AutoNovaDW;  --(For production systems, we would never drop a database like this. I am doing it here because this is a repeatable development setup)
+END;
+GO
+
+-- Create Database
+CREATE DATABASE AutoNovaDW;
+GO
+
+-- Use Database
+USE AutoNovaDW;
+GO
